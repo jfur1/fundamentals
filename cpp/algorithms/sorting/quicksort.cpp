078024c30f2a46110c5ab6afa87e60ae30066b75
John Furlong <johnfurlong24@gmail.com>
2021-06-02 14:30:38 -0600

    Add partition function using first element as pivot

--- cpp/algorithms/sorting/quicksort.cpp
+++ cpp/algorithms/sorting/quicksort.cpp
@@ -8,15 +8,26 @@ void swap(int* a, int* b){
     *b = t;
 }
 
+typedef void (* partition)(int arr[], int low, int high);
+
 /*
     Function takes the LAST element to be the pivot.
     Places the pivot at its correct position in the sorted array,
     and places all elements less than pivot to the left of pivot,
     and places all elements greater than pivot to the right of pivot.
 */
-int partitionLast(int arr, int low, int high){
+int partitionLast(int arr[], int low, int high){
+    int pivot = arr[high];
+    int i = (low - 1);
 
-
+    for(int j = low; j <= high - 1; j++){
+        if(arr[j] < pivot){
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i+1], &arr[high]);
+    return (i + 1);
 }
 
 /*
@@ -25,9 +36,18 @@ int partitionLast(int arr, int low, int high){
     and places all elements less than pivot to the left of pivot,
     and places all elements greater than pivot to the right of pivot.
 */
-int partitionFirst(int arr, int low, int high){
-
-
+int partitionFirst(int arr[], int low, int high){
+    int pivot = arr[low];
+    int i = low + 1;
+    
+    for(int j = low + 1; j <= high; j++){
+        if(arr[j] < pivot){
+            swap(&arr[i], &arr[j]);
+            i++;
+        }
+    }
+    swap(&arr[low], &arr[i-1]);
+    return (i - 1);
 }
 
 /*
@@ -36,9 +56,9 @@ int partitionFirst(int arr, int low, int high){
     and places all elements less than pivot to the left of pivot,
     and places all elements greater than pivot to the right of pivot.
 */
-int partitionMedian(int arr, int low, int high){
+int partitionMedian(int arr[], int low, int high){
 
-
+    return 0;
 }
 
 /*
@@ -47,9 +67,20 @@ int partitionMedian(int arr, int low, int high){
     and places all elements less than pivot to the left of pivot,
     and places all elements greater than pivot to the right of pivot.
 */
-int partitionRandom(int arr, int low, int high){
+int partitionRandom(int arr[], int low, int high){
 
+    return 0;
+}
 
+void quicksort(int arr[], int low, int high, int (* partition)(int arr[], int low, int high)){
+    if(low < high){
+        // pi is the partitioning index, arr[p] is now at the right place
+        int pi = partition(arr, low, high);
+
+        // Seperately sort elements before partition and after partition
+        quicksort(arr, low, pi - 1, partition);
+        quicksort(arr, pi + 1, high, partition);
+    }
 }
 
 
@@ -61,6 +92,28 @@ void printArray(int arr[], int size){
 }
 
 int main(int argc, char* argv[]){
+    // cout << "argc: " << argc << endl;
+    // cout << "argv[1]: " << argv[1] << endl;
+
+    int arr[] = { 5, 3, 7, 9, 1, 2, 4, 8, 6 };
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Given array: \n";
+    printArray(arr, arr_size);
+    
+    if(argc > 1){
+        if(string(argv[1]) ==  "last")
+            quicksort(arr, 0, arr_size - 1, &partitionLast);
+        else if(string(argv[1]) ==  "first")
+            quicksort(arr, 0, arr_size - 1, &partitionFirst);
+        else if(string(argv[1]) ==  "median")
+            quicksort(arr, 0, arr_size - 1, &partitionMedian);
+        else if(string(argv[1]) ==  "random")
+            quicksort(arr, 0, arr_size - 1, &partitionRandom);
+    }
+
+    cout << "Sorted array: \n";
+    printArray(arr, arr_size);
 
     return 0;
 }
