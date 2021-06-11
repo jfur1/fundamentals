f8ed9e177f36d08a4f31b97afd79045c51925945
John Furlong <johnfurlong24@gmail.com>
2021-06-11 10:03:34 -0600

    Add function prototypes for bits.c :

---
+++ cpp/data-structures/5-bit-manipulation/bits.h
@@ -0,0 +1,26 @@
+#ifndef BITS_H
+#define BITS_H
+
+int getBit(int num, int i);
+
+int setBit(int num, int i);
+
+int clearBit(int num, int i);
+
+int bitInsertion(int N, int M, int i, int j);
+
+void binaryToString(double num);
+
+int flipBit(int num);
+
+int nextSmallest(int num);
+int nextLargest(int num);
+void nextNumber(int num);
+
+int conversion(int n1, int n2);
+
+void pairwiseSwap(int num);
+
+void drawLine(int screen[], int width, int x1, int x2, int y);
+
+#endif
