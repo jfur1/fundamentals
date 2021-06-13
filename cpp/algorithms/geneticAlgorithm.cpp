6049ec1a0386d393acb10355cebacdf2b137c4dd
John Furlong <johnfurlong24@gmail.com>
2021-06-13 12:34:28 -0600

    helper functions for genetic algorithm

--- cpp/algorithms/geneticAlgorithm.cpp
+++ cpp/algorithms/geneticAlgorithm.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+// Number of individuals in each generation
+#define POPULATION_SIZE 100
+
+// Valid Genes
+const string GENES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOP"\
+"QRSTUVWXYZ 1234567890, .-;:_!\"#%&/()=?@${[]}";
+
+// Function to generate random numbers in given range 
+int random_num(int start, int end)
+{
+    int range = (end-start)+1;
+    int random_int = start+(rand()%range);
+    return random_int;
+}
+  
+// Create random genes for mutation
+char mutated_genes()
+{
+    int len = GENES.size();
+    int r = random_num(0, len-1);
+    return GENES[r];
+}
+
 int main(){
 
     return 0;
